1ca0f6775c529b6c69d6ecc35db5c4ec9b634aab
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-16 14:20:15 +0900

    Container追加

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
+import { Container, Row, Col } from "reactstrap";
+import SideNav from "./components/SideNav";
+import MovieList from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <Row>
+          <Col xs={12} sm={4}>
+            <SideNav />
+          </Col>
+          <Col xs={12} sm={8}>
+            <MovieList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
