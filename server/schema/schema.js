0947e18d44ff2592c789da47618a4cf1e4709ce3
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-16 11:27:29 +0900

    データ追加のメソッド作成

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -28,6 +28,28 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+//データを追加するため
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addMovie: {
+      type: MovieType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        let movie = new Movie({
+          name: args.name,
+          genre: args.genre,
+        });
+        return movie.save();
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
