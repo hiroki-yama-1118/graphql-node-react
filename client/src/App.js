e6054b5ccda0d0094e35811874cb82e21b1f8a26
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-16 14:43:03 +0900

    apolloprovider適用

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import Header from "./components/Header";
 import { Container, Row, Col } from "reactstrap";
 import SideNav from "./components/SideNav";
 import MovieList from "./components/MovieList";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphq",
+});
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Container>
-        <Row>
-          <Col xs={12} sm={4}>
-            <SideNav />
-          </Col>
-          <Col xs={12} sm={8}>
-            <MovieList />
-          </Col>
-        </Row>
-      </Container>
+      {/* プロバイダーに参照したいデータを保持 */}
+      <ApolloProvider client={client}>
+        <Container>
+          <Row>
+            <Col xs={12} sm={4}>
+              <SideNav />
+            </Col>
+            <Col xs={12} sm={8}>
+              <MovieList />
+            </Col>
+          </Row>
+        </Container>
+      </ApolloProvider>
     </div>
   );
 }
