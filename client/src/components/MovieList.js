4dd8efbef9716f6a5ce4431d418a9db1b49080ba
hiroki-yama-1118 <hiroki.yamamoto@ralus-patners.co.jp>
2022-02-16 14:42:47 +0900

    テーブル作成

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Card, CardBody, Table } from "reactstrap";
 
 const MovieList = () => {
-  return <div>MovieList</div>;
+  return (
+    <Card>
+      <CardBody>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>タイトル</th>
+              <th>ジャンル</th>
+              <th>監督</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>タイトル</td>
+              <td>ジャンル</td>
+              <td>監督</td>
+            </tr>
+          </tbody>
+        </Table>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default MovieList;
